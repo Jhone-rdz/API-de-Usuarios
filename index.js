81c3f6bab3680d93d14ce2d56ed532fbd8c89e09
jhone-rdz <jhone.rodrigues51@gmail.com>
2025-07-10 19:26:31 -0300

    add banco de dados

--- index.js
+++ index.js
@@ -1,50 +1,65 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
 const PORT = 3000;
-
-let usuarios = [
-  { id: 1, nome: "Maria" },
-  { id: 2, nome: "João" }
-];
 
 
 app.use(express.json());
 
-app.get('/usuarios', (req, res) => {
-   res.json(usuarios);
+app.get('/usuarios', async (req, res) => {
+  const result = await pool.query('SELECT * FROM usuarios');
+  res.json(result.rows);
 });
-app.post('/usuarios', (req, res) => {
-   const { nome } = req.body;
-  const novoUsuario = {
-    id: usuarios.length + 1,
-    nome
-  };
-  usuarios.push(novoUsuario);
-  res.status(201).json(novoUsuario);
+
+app.post('/usuarios', async (req, res) => {
+  const { nome } = req.body;
+  const result = await pool.query(
+    'INSERT INTO usuarios (nome) VALUES ($1) RETURNING *',
+    [nome]
+  );
+  res.status(201).json(result.rows[0]);
 });
-app.put('/usuarios/:id', (req, res) => {
-   const { id } = req.params;
+
+app.put('/usuarios/:id', async (req, res) => {
+  const { id } = req.params;
   const { nome } = req.body;
 
-  const usuario = usuarios.find(u => u.id === parseInt(id));
-  if (!usuario) {
-    return res.status(404).json({ erro: "Usuário não encontrado" });
-  }
+  try {
+    const result = await pool.query(
+      'UPDATE usuarios SET nome = $1 WHERE id = $2 RETURNING *',
+      [nome, id]
+    );
 
-  usuario.nome = nome;
-  res.json(usuario);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ erro: 'Erro ao atualizar usuário', detalhe: err.message });
+  }
 });
-app.delete('/usuarios/:id', (req, res) => {
+
+app.delete('/usuarios/:id', async (req, res) => {
   const { id } = req.params;
-  const index = usuarios.findIndex(u => u.id === parseInt(id));
-  
-  if (index === -1) {
-    return res.status(404).json({ erro: "Usuário não encontrado" });
-  }
 
-  const usuarioRemovido = usuarios.splice(index, 1);
-  res.json({ mensagem: "Usuário removido", usuario: usuarioRemovido[0] });
+  try {
+    const result = await pool.query(
+      'DELETE FROM usuarios WHERE id = $1 RETURNING *',
+      [id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+
+    res.json({ mensagem: 'Usuário removido', usuario: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ erro: 'Erro ao remover usuário', detalhe: err.message });
+  }
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
